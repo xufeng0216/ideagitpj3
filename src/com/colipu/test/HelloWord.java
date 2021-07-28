b034a35426908f80066db9de852cdabbc664b05c
xufeng0216 <twtdh@163.com>
2021-07-28 18:03:04 +0800

    second modify

--- src/com/colipu/test/HelloWord.java
+++ src/com/colipu/test/HelloWord.java
@@ -6,5 +6,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println(HELLO_WORLD );
         System.out.println(HELLO_WORLD );
+        System.out.println(HELLO_WORLD );
     }
 }
