c10de1d0abb893ec1cecf12644f2d407d10c755d
xufeng <xufeng@colipu.com>
2021-07-28 17:45:10 +0800

    first commit to github

---
+++ src/com/colipu/test/HelloWord.java
@@ -0,0 +1,10 @@
+package com.colipu.test;
+
+public class HelloWord {
+    public static final String HELLO_WORLD = "HelloWorld!";
+
+    public static void main(String[] args) {
+        System.out.println(HELLO_WORLD );
+        System.out.println(HELLO_WORLD );
+    }
+}
